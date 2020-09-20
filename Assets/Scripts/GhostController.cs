5dc753a12d87f1cf94b57f91d0c02d9dfff5620c
David Bolis <50950141+DavidBolis261@users.noreply.github.com>
2020-09-20 20:42:55 +1000

    FirstCommit

---
+++ Assets/Scripts/GhostController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostController : MonoBehaviour
+{
+    //public Renderer rend;
+    public GameObject[] DeadGhosts;
+    public GameObject[] AliveGhosts;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        DeadGhosts = GameObject.FindGameObjectsWithTag("DeadGhost");
+        AliveGhosts = GameObject.FindGameObjectsWithTag("AliveGhost");
+        StartCoroutine(ActivationRoutine());
+
+        
+    }
+
+    private void GhostPMatch()
+    {
+        
+    }
+
+    private IEnumerator ActivationRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3);
+            foreach (GameObject ghost in AliveGhosts)
+            {
+                ghost.GetComponent<SpriteRenderer>().enabled = false;
+
+            }
+
+            yield return new WaitForSeconds(3);
+            foreach (GameObject ghost in DeadGhosts)
+            {
+                ghost.GetComponent<SpriteRenderer>().enabled = false;
+
+            }
+
+
+
+            yield return new WaitForSeconds(3);
+            foreach (GameObject ghost in AliveGhosts)
+            {
+                ghost.GetComponent<SpriteRenderer>().enabled = true;
+
+            }
+
+            yield return new WaitForSeconds(3);
+            foreach (GameObject ghost in DeadGhosts)
+            {
+                ghost.GetComponent<SpriteRenderer>().enabled = true;
+
+            }
+        }
+        
+       
+    }
+
+}
---
+++ Assets/Scripts/PacmanMove.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanMove : MonoBehaviour
+{
+    public float speed = 0.4f;
+    Vector2 destination = Vector2.zero;
+
+    
+    public AudioSource audiosource;
+    public AudioSource PacManEating;
+    void Start()
+    {
+       
+        destination = transform.position;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Pallet")
+        {
+           
+            PacManEating.Play();   
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            if (!audiosource.isPlaying)
+            {
+                audiosource.Play();
+            }
+        } else
+        {
+            audiosource.Stop();
+        }
+      
+
+        Vector2 p = Vector2.MoveTowards(transform.position, destination, speed);
+        GetComponent<Rigidbody2D>().MovePosition(p);
+        if ((Vector2)transform.position == destination)
+        {
+            if ((Vector2)transform.position == destination)
+            {
+                if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
+                {
+                    destination = (Vector2)transform.position + Vector2.up;
+                    
+     
+                }
+                    
+                if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
+                {
+                    destination = (Vector2)transform.position + Vector2.right;
+                   
+                }
+                    
+                if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
+                {
+                    destination = (Vector2)transform.position - Vector2.up;
+                }
+                   
+                if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
+                {
+                    destination = (Vector2)transform.position - Vector2.right;
+                }
+                   
+            }
+
+        }
+        Vector2 dir = destination - (Vector2)transform.position;
+        GetComponent<Animator>().SetFloat("DirX", dir.x);
+        GetComponent<Animator>().SetFloat("DirY", dir.y);
+    }
+    bool valid(Vector2 dir)
+    {
+       
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+        return (hit.collider == GetComponent<Collider2D>());
+    }
+}
